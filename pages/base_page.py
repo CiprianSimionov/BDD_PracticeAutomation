8ccda24ff9ecdb18b3c3ee043921f0f2dac780fc
ciprian simionov <simionov.ciprian@gmail.com>
2025-05-27 14:20:48 +0300

    updated base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,3 +4,5 @@ from browser import Browser
 class BasePage(Browser):
     def access_login_page(self):
         self.chrome.get("https://practicetestautomation.com/practice-test-login/")
+    def access_elements_page(self):
+        self.chrome.get("https://demoqa.com/")
