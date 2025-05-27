4cab6a3f6748f95b11ed1c23a0c61e8926ea9022
ciprian simionov <simionov.ciprian@gmail.com>
2025-05-27 14:17:39 +0300

    updated environment.py

--- environment.py
+++ environment.py
@@ -1,4 +1,5 @@
 from pages.login_page import LoginPage
+from pages.alerts_frame_page import Alerts_Frame_Page
 from pages.base_page import BasePage
 from browser import Browser
 
@@ -7,6 +8,7 @@ def before_all(context):
     context.browser = Browser()
     context.base_page = BasePage()
     context.login_page = LoginPage()
+    context.alerts_frame_page = Alerts_Frame_Page()
 
 def after_all(context):
     context.browser.close()
