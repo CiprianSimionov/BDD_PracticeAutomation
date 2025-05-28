39b52fd714ca172cae1555cd9ff394a69981efcb
ciprian simionov <simionov.ciprian@gmail.com>
2025-05-28 14:28:30 +0300

    updated environment.py

--- environment.py
+++ environment.py
@@ -1,5 +1,6 @@
 from pages.login_page import LoginPage
 from pages.alerts_frame_page import Alerts_Frame_Page
+from pages.demoblaze_page import Demoblaze
 from pages.base_page import BasePage
 from browser import Browser
 
@@ -9,6 +10,7 @@ def before_all(context):
     context.base_page = BasePage()
     context.login_page = LoginPage()
     context.alerts_frame_page = Alerts_Frame_Page()
+    context.demoblaze_page = Demoblaze()
 
 def after_all(context):
     context.browser.close()
